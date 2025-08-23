983d2d5187640b89190e6886b568e245e7e0e0fc
roshankrsu <roshankrsu@email.com>
2025-08-23 22:20:21 +0530

    first commit

---
+++ area.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int a;
+  cin>>a;
+  int area = a*a;
+  cout<<"the area of "<< a<<" is 2"<<area<<"\n";
+    return 0;
+}
---
+++ min_of_two.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+    int main(){
+        int a,b;
+        cin>>a;
+        cin>>b;
+        if(a<b){
+            cout<<a<<" is the small number\n";
+        }else
+        cout<<b<<" is the small number\n2";
+    }
