b92c3f87c9530a00ef8ffe3f7aa31e263a35a209
roshankrsu <roshankrsu@gmail.com>
2025-09-03 10:34:42 +0530

    sum of nth term

--- sum.cpp
+++ sum.cpp
@@ -1,15 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-
     int term,sum=0;
-    cout<<"Enter the term: ";
+    cout<<"Enter the nth term: ";
     cin>>term;
-    for (int i = 1; i <=term; i++)
-    {
-        sum=sum+i;
+    for(int i = 1; i<=term; i++){
+        sum = sum + i;
     }
-    cout<<"sum of "<<term<<" term is "<<sum;
-
-    return 0;
+    cout<<"the sum of "<<term<<" term is "<<sum;
 }
