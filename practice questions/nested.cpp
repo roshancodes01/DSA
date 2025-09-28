e2b47fd58da4d5c17c4d9afdd82d1c4f10d5cd8d
roshankrsu <roshankrsu@gmail.com>
2025-09-28 16:27:23 +0530

    nested loop

---
+++ practice questions/nested.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n = 5;
+    for(int i = 1; i<=n; i++){
+      int x = 4;
+      for(int i=1; i<=x;i++){
+         cout<<"*";
+      }
+      cout<<endl;
+    } 
+    return 0;
+}
