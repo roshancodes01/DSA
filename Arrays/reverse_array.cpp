d09a25894eba1b45aa7f002cc69aafcd6dc0cf11
roshankrsu <roshankrsu@gmail.com>
2025-10-02 13:49:14 +0530

    reverse array

---
+++ Arrays/reverse_array.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std; 
+
+void reverseArray(int arr[], int sz){
+    int start = 0, end = sz-1;
+    while (start < end)
+    {
+       swap(arr[start], arr[end]);
+       start++;
+       end--;
+    }
+    
+}
+
+int main(){
+    int arr[] = {3,5,67,7,34};
+    int sz = 5;
+
+
+    reverseArray(arr,sz);
+
+    for(int i = 0; i<sz; i++){
+        cout<<arr[i]<<" ";
+    }
+    
+    cout<<endl;
+
+    return 0;
+}
