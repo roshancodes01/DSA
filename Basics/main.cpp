fabd5c322e45b435fae26cc68880bd8534f7742c
roshankrsu <roshankrsu@gmail.com>
2025-10-02 10:25:54 +0530

    removed

--- Basics/main.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    string name;
-    cout<<"Enter Repo Owner Name: ";
-    cin>>name;
-    cout<<"Welcome "<<name;
-}
--- Basics/ternary.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-    int n = -7;
-
-    cout<<(n >= 0?"positive":"negative");
-    return 0;
-}
