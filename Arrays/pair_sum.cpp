b81f285572edd03689d40a6dace81f815f18d035
roshankrsu <roshankrsu@gmail.com>
2025-10-03 21:43:53 +0530

    pair sum solved using brut force and optimal approach

---
+++ Arrays/pair_sum.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+using namespace std; 
+        // brut force approach
+       //o(n^2) time complexity
+// vector<int> pairSum(vector<int> nums, int target){
+//       vector<int> ans;
+//       int n = nums.size();
+
+//       for(int i=0; i<n; i++){
+//         for(int j=i+1; j<n; j++){
+//             if(nums[i] + nums[j] == target) {
+//                 ans.push_back(i);
+//                 ans.push_back(j);
+//                 return ans;
+//             }
+//         }
+//       }
+//     }
+      //optimal approach
+//o(n) time complexity
+ vector<int> pairSum(vector<int> nums, int target){
+      vector<int> ans;
+      int n = nums.size();     
+    int i = 0, j = n-1;
+  while(i<j){
+    int pairSum = nums[i] + nums[j];
+    if(pairSum > target){
+        j--;
+    }else if(pairSum < target){
+         i++;
+    }else{
+        ans.push_back(i);
+        ans.push_back(j);
+        return ans;
+    }
+  }
+  return ans;
+ }
+
+
+
+
+int main(){
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 26;
+
+
+    vector<int> ans = pairSum(nums, target);
+    cout<< ans[0] << ", "<<ans[1]<<endl;
+
+}
